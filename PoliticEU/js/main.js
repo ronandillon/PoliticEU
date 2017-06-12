c5b94b3d1ee76cf44f88be0853372d5500cf5d49
Ronan <bayrule@hotmail.com>
2017-06-12 03:26:48 +0100

    Commit 2: 12/06/2017
    Changes: Modal added on click.
    To add, parliament-svg to make an svg for a parliament of a given size
    which is needed as countries have differently sized governments.

--- PoliticEU/js/main.js
+++ PoliticEU/js/main.js
@@ -1,37 +1,59 @@
 //Width and height
-			var w = 800;
-			var h = 600;
+var w = 800;
+var h = 600;
 
-			//Define map projection
+//Define map projection
+var projection = d3.geo.mercator()
+                       .center([ 13, 52 ])
+                       .translate([ w/2.3, h/1.6 ])
+                       .scale([ w/1.5 ]);//Scale of the map.
+
+//Define path generator
+var path = d3.geo.path().projection(projection);
+
+//Create SVG
+var svg = d3.select("#container")
+            .append("svg")
+            .attr("width", w)
+            .attr("height", h);
+
+//Load in GeoJSON data
+d3.json("countries.json", function(json) {
+
+    //Bind data and create one path per GeoJSON feature
+    svg.selectAll("path")
+       .data(json.features)
+       .enter()
+       .append("path")
+       .attr("d", path)
+       .attr("stroke", "rgba(0,0,0, 0.2)")
+       .attr("fill", "rgba(8, 81, 156, 0.6)")
+       .attr("onclick", "open_modal(__data__.properties.sovereignt)");//formal_en another choice for the name. Longer. More....formal.
+
+});
 
 
-			var projection = d3.geo.mercator()
-								   .center([ 13, 52 ])
-								   .translate([ w/2.3, h/1.6 ])
-								   .scale([ w/1.5 ]);//Scale of the map.
+var modal = document.getElementById('myModal');
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
 
-			//Define path generator
-			var path = d3.geo.path().projection(projection);
+// When the user clicks the button, open the modal
+open_modal = function(name) {
+    $("#m-header").text("Dail Eireann");//Will change depending on country. Tabs need to be added for multiple houses of government
+    $("#m-footer").text(name);
+    modal.style.display = "block";
+}
 
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
 
-			//Create SVG
-			var svg = d3.select("#container")
-						.append("svg")
-						.attr("width", w)
-						.attr("height", h);
-
-			//Load in GeoJSON data
-			d3.json("countries.json", function(json) {
-                console.log(path);
-
-			    //Bind data and create one path per GeoJSON feature
-				svg.selectAll("path")
-				   .data(json.features)
-				   .enter()
-				   .append("path")
-				   .attr("d", path)
-				   .attr("stroke", "rgba(0,0,0, 0.2)")
-				   .attr("fill", "rgba(8, 81, 156, 0.6)")
-                   .attr("onclick", "console.log(__data__.properties.sovereignt)");//formal_en another choice for the name. Longer. More....formal.
-
-			});
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
