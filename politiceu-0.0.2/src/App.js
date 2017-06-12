750d4c197bd7ec787687f7270b71954291214575
Ronan <bayrule@hotmail.com>
2017-06-12 21:23:16 +0100

    Move to React
    Decided to use React for a few reasons but mainly to get some practice
    using React.
    Created a basic React App for now. Next up is moving what I had
    previously over to the new React App.

---
+++ politiceu-0.0.2/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ParliamentSVG from 'parliament-svg';
+import './index.css';
+
+class App extends React.Component{
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img  className="App-logo" alt="logo" />
+          <h2>Welcome to React</h2>
+        </div>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
