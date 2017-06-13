988762f52824c1600f0f38c66296b88243855403
Ronan <bayrule@hotmail.com>
2017-06-13 17:07:01 +0100

    Introuction of Parliament-SVG
    Implementation Parliament SVG. Currently hardcoded for Dail Eireann but
    once a back-end is implemented each individual country will have their
    own data.
    Further implementation will include adding more houses of government.
    E.g. The Seanad in Ireland's case.

--- politiceu-0.0.2/src/App.js
+++ politiceu-0.0.2/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import ParliamentSVG from 'parliament-svg';
+import toStr from 'virtual-dom-stringify';
 import d3 from 'd3';
 import $ from 'jquery';
 import svg from 'svg';
@@ -37,7 +38,6 @@ var svg1 = d3.select("#container").append("svg").attr("width", w).attr("height",
 
 //Load in GeoJSON data
 d3.json("countries.json", function(json) {
-    console.log(json);
     //Bind data and create one path per GeoJSON feature
     svg1.selectAll("path")
        .data(json.features)
@@ -46,19 +46,9 @@ d3.json("countries.json", function(json) {
        .attr("d", path)
        .attr("stroke", "rgba(0,0,0, 0.2)")
        .attr("fill", "rgba(8, 81, 156, 0.6)")
-       .attr("onclick", "alert((__data__.properties.sovereignt))");//formal_en another choice for the name. Longer. More....formal.
+       .attr("onclick", "open_modal(__data__.properties.formal_en)");//formal_en another choice for the name. Longer. More....formal.
 
 });
-
-$("path").hover(
-    function(event){
-        alert();
-    },
-    function(event){
-
-    }
-);
-
 
 var modal = document.getElementById('myModal');
 // Get the button that opens the modal
@@ -70,9 +60,58 @@ var span = document.getElementsByClassName("close")[0];
 var open_modal = function(name) {
     $("#m-header").text("Dail Eireann");//Will change depending on country. Tabs need to be added for multiple houses of government
     $("#m-footer").text(name);
+    var parties={
+    "Fine Gael": {
+        "seats": 50,
+        "colour": "blue"
+    },
+    "Independent": {
+        "seats": 7,
+        "colour": "grey"
+    },
+    "Fianna Fail": {
+        "seats": 44,
+        "colour": "#00FF00"
+    },
+    "Sine Fein": {
+        "seats": 23,
+        "colour": "#006400"
+    },
+    "Labour": {
+        "seats": 7,
+        "colour": "red"
+    },
+    "AAA-PBP": {
+        "seats": 6,
+        "colour": "yellow"
+    },
+    "I4C": {
+        "seats": 4,
+        "colour": "pink"
+    },
+    "Green": {
+        "seats": 2,
+        "colour": "#228B22"
+    },
+    "Soc Dem": {
+        "seats": 2,
+        "colour": "purple"
+    },
+    "Independents": {
+        "seats": 12,
+        "colour": "grey"
+    }
+    };
+    var seatCount=631;
+    const svg = ParliamentSVG(parties, seatCount);
+    var svg1 = toStr(svg);
+    console.log(svg1);
+    $("#m-body").append(svg1);
+    //document.getElementById('m-body').append(svg1);
+    //$("#m-body").value(svg);
     modal.style.display = "block";
-}
-
+};
+open_modal("Ireland");
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
     modal.style.display = "none";
@@ -80,7 +119,7 @@ span.onclick = function() {
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-    if (event.target == modal) {
+    if (event.target === modal) {
         modal.style.display = "none";
     }
 }
